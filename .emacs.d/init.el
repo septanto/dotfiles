01972cf703939d1a02a89df40a6285eb8a34140e
septanto <s.s.adi.nugroho@gmail.com>
2026-06-21 23:35:30 +0700

    update config emacs & niri

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,7 +62,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi-tinted)))
+ '(custom-enabled-themes '(leuven-dark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
