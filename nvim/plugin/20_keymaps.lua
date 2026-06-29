cd734913ed4887c8eda7842740bd093ad82ec6e4
septanto <s.s.adi.nugroho@gmail.com>
2026-06-29 10:32:20 +0700

    add delete other buffers keymap

--- nvim/plugin/20_keymaps.lua
+++ nvim/plugin/20_keymaps.lua
@@ -85,9 +85,19 @@ local new_scratch_buffer = function()
   vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(true, true))
 end
 
+local delete_other_buffers = function()
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if buf ~= current and vim.bo[buf].buflisted then
+      MiniBufremove.delete(buf)
+    end
+  end
+end
+
 nmap_leader('ba', '<Cmd>b#<CR>',                                 'Alternate')
 nmap_leader('bd', '<Cmd>lua MiniBufremove.delete()<CR>',         'Delete')
 nmap_leader('bD', '<Cmd>lua MiniBufremove.delete(0, true)<CR>',  'Delete!')
+nmap_leader('bs', delete_other_buffers,                          'Delete other buffers')
 nmap_leader('bs', new_scratch_buffer,                            'Scratch')
 nmap_leader('bw', '<Cmd>lua MiniBufremove.wipeout()<CR>',        'Wipeout')
 nmap_leader('bW', '<Cmd>lua MiniBufremove.wipeout(0, true)<CR>', 'Wipeout!')
